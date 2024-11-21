9d125a787a3267a10cf99d112bb3773331867b07
Abd766 <abdullahmasood766@gmail.com>
2024-11-21 14:31:48 +0530

    Added Counter-app

---
+++ React-counter-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Count is {}</h1>
+      <button>INCREASE</button>
+      <button>DECREASE</button>
+    </div>
+  );
+};
+export default App;
